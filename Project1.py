f816bb3ecb83857df918f82f61110f07d4fd22cc
m-wintersteen <muniwintersteen@gmail.com>
2020-01-29 21:52:04 -0700

    Created Git Repo

---
+++ Project1.py
@@ -0,0 +1,52 @@
+'''
+Jessica Jorgenson
+Matthew Wintersteen
+CSCI 347 Project 1
+Python Code for Data Analysis
+'''
+import numpy as np
+
+'''
+A function to compute the mean of a numerical, multidimensional data set
+input as a 2-dimensional numpy array
+'''
+def computeMean(arr):
+    return 0
+
+'''
+A function to compute the sample covariance between two attributes that
+are input as one-dimensional numpy vectors
+'''
+def computeCovar(v1, v2):
+    return 0
+
+'''
+A function to compute the correlation between that are input as
+one-dimensional numpy vectors
+'''
+def computeCorr(v1, v2):
+    return 0
+
+'''
+A function to range normalize a two-dimensional numpy array
+'''
+def rangeNorm(arr):
+    return normArr
+
+'''
+A function to standard normalize a two-dimensional numpy array
+'''
+def standardNorm(arr):
+    return normArr
+
+'''
+A function to compute the covariance matrix of a dataset
+'''
+def computeCovarMatrix(arr):
+    return covarMatrix
+
+'''
+A function to label-encode categorical data
+'''
+def labelEncode(arr)
+    return encodedArr
