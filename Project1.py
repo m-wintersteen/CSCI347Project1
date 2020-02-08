828e6c3c072552efad073d333562fbbea9b11ca0
m-wintersteen <muniwintersteen@gmail.com>
2020-02-08 14:03:23 -0700

    Final push

--- Project1.py
+++ Project1.py
@@ -384,6 +384,8 @@ def main():
 columns = ['symboling','normalized-losses','make','fuel-type','aspiration','num-of-doors','body-style','drive-wheels','engine-location','wheel-base','length','width','height','curb-weight','engine-type','num-of-cylinders','engine-size','fuel-system','bore','stroke','compression-ratio','horsepower','peak-rpm','city-mpg','highway-mpg','price']
 categorical = ['make','fuel-type','aspiration','num-of-doors','body-style','drive-wheels','engine-location','engine-type','num-of-cylinders','fuel-system']
 
-#testFunc()
+#TestFunc Runs all the python functions for Part 2
+testFunc()
+#Main runs the python data analysis needed for Part 3
 main()
 
